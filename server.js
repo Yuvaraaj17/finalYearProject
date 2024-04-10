2e203381c69da353efc9b34c2cb337886c1bdad3
yuvaraaj,s <yuvaraaj.s@emsibg.com>
2024-04-10 14:44:14 +0530

    changed port no

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const {WebSocketServer} = require('ws')
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8765 });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
