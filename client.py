f81d6557c95db5545ebc389e812c89e4bb91bcf0
Yuvaraaj17 <yuvaraaj5910@gmail.com>
2024-04-13 11:01:09 +0530

    added model

--- client.py
+++ client.py
@@ -4,17 +4,21 @@ from ultralytics import YOLO
 
 async def sending():
     async with websockets.connect("ws://localhost:8765") as websocket:
-        model = YOLO('yolov8n.pt')
+        model = YOLO('best.pt')
         result = model.predict(0,stream=True) #stream=True for Result generation
         for r in result:
             classnameTensor = r.boxes.cls #tensor type
             res =''
-            for i in classnameTensor:
-                classNumber = int(i.item())
-                className = r.names[classNumber]
-                res=res + f' {className}'
-            await websocket.send(res)
-            await websocket.recv()
+            if len(classnameTensor) != 0:
+                    i = classnameTensor[0]
+                    classNumber = int(i.item())
+                    className = r.names[classNumber]
+                    res=res + f' {className}'
+                    await websocket.send(res)
+                    await websocket.recv()
 async def main():
     await sending()
 asyncio.run(main())
+
+# print("hello")
+
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.yaml')
+if __name__ == '__main__':
 
-model.train(data='config.yaml',epochs=10)
+    model = YOLO('yolov8n.yaml')
+    result = model.train(data='config.yaml',amp=False,epochs=60,workers=2,batch=16)
